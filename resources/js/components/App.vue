ad2951dbe6c06fc76a22ab1daff409c01bf4f879
ElijahCoding <cuiyeqing960904@gmail.com>
2020-01-14 12:43:15 +0300

    routes set up , vue-router vuex set up

---
+++ resources/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <router-view :key="$route.fullPath"></router-view>
+</template>
+
+<script>
+    export default {
+        name: "App.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
